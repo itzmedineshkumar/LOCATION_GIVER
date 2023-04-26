1ad118b6c4380d09f6ce7ea67183f15ecfb9067b
itzmedineshkumar <dinesh.100314@mygyanvihar.com>
2023-04-26 17:17:14 +0530

    VARIABLES UPDATED

--- req.py
+++ req.py
@@ -1,19 +1,19 @@
 import requests
 key = '2472c2eb627da1c41290acbe6bf4f203'
-cityname = input("ENTER YOUR CITY NAME FOR LOCATION >>> ")
-aq = f"http://api.openweathermap.org/geo/1.0/direct?q={cityname}&appid={key}"
-a= requests.get(aq).json()
-b=len(a)
+city = input("ENTER YOUR CITY NAME FOR LOCATION >>> ")
+aq = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={key}"
+data= requests.get(aq).json()
+b=len(data)
 print(b)
 if b==1:
-    longitude=a[0]['lon']
-    print(longitude)
-    latitude=a[0]['lat']
-    print(latitude)
-    country=a[0]['country']
-    hindi=a[0]['local_names'].get('hi',None)
-    print(hindi)
-    print(f"YOU HAD ENTERED FOR {cityname} FOR {country} AND ITS LONGITUDE IS {longitude} , ITS LATITUDE IS {latitude} AND CITY NAME IN HINDI IS {hindi}")
+    lon=data[0]['lon']
+    print(lon)
+    lat=data[0]['lat']
+    print(lat)
+    country=data[0]['country']
+    motherlang=data[0]['local_names'].get('hi',None)
+    print(motherlang)
+    print(f"YOU HAD ENTERED FOR {city} FOR {country} AND ITS LONGITUDE IS {lon} , ITS LATITUDE IS {lat} AND CITY NAME IN HINDI IS {motherlang}")
 else:
     print("CITY NAME NOT FOUND")
 
